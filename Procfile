b5e7fbdcaaa18af3d555e86f7ba1ccf0680bb0dd
Quint <quint@github.local>
2026-04-28 17:45:42 -0500

    Harden Render startup command

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python -m gunicorn contabilidad.wsgi:application --bind 0.0.0.0:$PORT --log-file -
--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-python manage.py migrate --no-input
-gunicorn contabilidad.wsgi:application \
+exec python -m gunicorn contabilidad.wsgi:application \
   --bind 0.0.0.0:${PORT:-8000} \
   --log-file -
