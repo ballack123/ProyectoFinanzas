ad466780ba19d7032af1d6195544abe6ae5683e5
Quint <quint@github.local>
2026-04-28 18:09:31 -0500

    fix: cambiar a puerto 465 SSL para correos en Render

--- contabilidad/settings.py
+++ contabilidad/settings.py
@@ -109,8 +109,9 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Configuración de Envío de Correos (SMTP Gmail)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
