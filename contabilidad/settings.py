25f776faaea10ec308c76d05e9e59b753a2718ba
angehv <angelhuamanivarg@gmail.com>
2026-04-29 05:29:15 -0500

    undo

--- contabilidad/settings.py
+++ contabilidad/settings.py
@@ -93,10 +93,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
-# Reemplaza directamente aquí con tus datos:
-EMAIL_HOST_USER = 'angelhuamanivarg0206@gmail.com' 
-EMAIL_HOST_PASSWORD = 'zxkr rcvi chqo ixgv' 
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#a
