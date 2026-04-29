637d16702e51fc5277d8c8b2ea454a81a613b9db
angehv <angelhuamanivarg@gmail.com>
2026-04-29 05:19:04 -0500

    prueba

--- contabilidad/settings.py
+++ contabilidad/settings.py
@@ -93,9 +93,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+
+# Reemplaza directamente aquí con tus datos:
+EMAIL_HOST_USER = 'angelhuamanivarg0206@gmail.com' 
+EMAIL_HOST_PASSWORD = 'zxkr rcvi chqo ixgv' 
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-#a
