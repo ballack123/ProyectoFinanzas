ca4f7d08844223c9a1600acd9b2315399311a839
ballack123 <quintanillaballack2@gmail.com>
2026-04-28 20:09:58 -0500

    Revert "Configure production email delivery"
    This reverts commit 846be02f517a90ba2be4cdc555b830e1389af45d.

--- contabilidad/settings.py
+++ contabilidad/settings.py
@@ -113,8 +113,6 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
-EMAIL_TIMEOUT = int(os.getenv('EMAIL_TIMEOUT', '20'))
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
