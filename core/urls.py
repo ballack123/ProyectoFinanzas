81d5aba685dbc184f6c1b516e2e37a35ac301530
ballack123 <quintanillaballack2@gmail.com>
2026-04-28 20:10:11 -0500

    Revert "Add accounting chatbot assistant"
    This reverts commit 781e61f0508bbd8400a74c53264f40c4c770151b.

--- core/urls.py
+++ core/urls.py
@@ -27,5 +27,4 @@ urlpatterns = [
     path('reportes/estado-resultados/', views.estado_resultados, name='estado_resultados'),
     path('reportes/balance-general/', views.balance_general, name='balance_general'),
     path('reporte-completo/', views.reporte_completo, name='reporte_completo'),
-    path('asientos/chatbot/', views.chatbot_api, name='chatbot_api'),
 ]
